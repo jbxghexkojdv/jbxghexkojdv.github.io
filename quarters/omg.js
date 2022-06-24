22ffda18a82593b0ae7a29af637f595a2df73d36
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-24 14:38:33 -0500

    browserify

--- quarters/omg.js
+++ quarters/omg.js
@@ -3,48 +3,48 @@ let days = 0;
 const leapYear = new Date().getFullYear() % 4 == 0;
 switch(month)
 {
-    case 11:
-        days += 31;
-    case 10:
-        days += 30;
-    case 9:
-        days += 31;
-    case 8:
-        days += 30;
-    case 7:
-        days += 31;
-    case 6:
-        days += 31;
-    case 5:
-        days += 30;
-    case 4:
-        days += 31;
-    case 3:
-        days += 30;
-    case 2:
-        days += 31;
-    case 1:
-        if(leapYear)
-        {
-        	    days += 29;
-        	}
-        	else
-        	{
-        		   days += 28;
-        	}
-        	break;
+  case 11:
+    days += 31;
+  case 10:
+    days += 30;
+  case 9:
+    days += 31;
+  case 8:
+    days += 30;
+  case 7:
+    days += 31;
+  case 6:
+    days += 31;
+  case 5:
+    days += 30;
+  case 4:
+    days += 31;
+  case 3:
+    days += 30;
+  case 2:
+    days += 31;
+  case 1:
+    if(leapYear)
+    {
+      days += 29;
+    }
+    else
+    {
+      days += 28;
+    }
+    break;
 }
 days += new Date().getDate();
 let scale = days;
 if(leapYear)
 {
-    scale /= 366;
+  scale /= 366;
 }
 else
 {
-	   scale /= 365;
+  scale /= 365;
 }
 scale **= -1;
 scale *= 18.6164383561644;
 price = scale + 30;
-console.log(`The price of a ${new Date().getFullYear()} quarter is currently ${Math.round(price)} cents`);
+document.getElementById("a").innerHTML = `The price of a ${new Date().getFullYear()} quarter is currently ${Math.round(price)} cents`;
