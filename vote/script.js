a216671864ae5ddb1f2a9b9f2a0f6f439f630a87
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-27 00:10:05 -0500

    Create script.js, include the base code

---
+++ vote/script.js
@@ -0,0 +1,72 @@
+function approval()
+{
+  
+}
+
+function rankedchoice()
+{
+  
+}
+
+function fptp()
+{
+  
+}
+
+function queryStringParser(url)
+{
+  let query = url.split("?")[1];
+  let args = query.split("&");
+  let retval = {};
+  for (let i of args)
+  {
+    let kv = i.split("=");
+    retval[kv[0]] = kv[1];
+  }
+  return retval;
+}
+
+const optionsobj = queryStringParser(window.location.href);
+
+switch(optionsobj.system)
+{
+  case "approval":
+    approval();
+    break;
+  case "rc":
+    rankedchoice();
+    break;
+  case "fptp":
+    fptp();
+    break;
+  default:
+    rankedchoice();
+}
+
+let title = document.getElementsByTagName("title")[0];
+
+let month;
+let year;
+switch(Number(optionsobj.time) % 6)
+{
+  case 0:
+    month = "September";
+    break;
+  case 1:
+    month = "November";
+    break;
+  case 2:
+    month = "January";
+    break;
+  case 3:
+    month = "March";
+    break;
+  case 4:
+    month = "May";
+    break;
+  case 5:
+    month = "July";
+}
+
+year = Math.floor((Number(optionsobj.time)-2)/6)+2022;
+title += " for the project of " + month + " " + year;
