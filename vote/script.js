03991ec4bcf2fc2a94c44659cad6d2b11c30db0f
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-28 15:45:17 -0500

    add function to create table

--- vote/script.js
+++ vote/script.js
@@ -5,6 +5,35 @@ function createElem(tag = "p", parent = document.getElementsByTagName("body")[0]
   return elem;
 }
 
+function createTable(rows = 0, cols = 0, parent = document.getElementsByTagName("body")[0])
+{
+  let table = createElem("table", parent);
+  table.style.border = "3px solid black";
+  let retval = 
+  {
+    table: table,
+    rows: [],
+    cells: []
+  };
+  for(let i = 0; i < rows; i++)
+  {
+    // It is a woman's right to have an abortion
+    // Fuck the Supreme Court
+    let roe = createElem("tr", table);
+    retval.rows.push(roe);
+    roe.style.border = "3px solid black";
+    let datas = [];
+    for(let j = 0; j < rows; j++)
+    {
+      let cell = createElem("td", roe);
+      cell.style.border = "3px solid black";
+      datas.push(cell);
+    }
+    retval.cells.push(datas);
+  }
+  return retval;
+}
+
 function approval()
 {
   let message = createElem();
