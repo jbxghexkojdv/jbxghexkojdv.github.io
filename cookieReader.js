7f805709046f2c4c7cb5de74c68ceab54c4d450b
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-30 22:18:59 -0500

    Update cookieReader.js

--- cookieReader.js
+++ cookieReader.js
@@ -1,4 +1,19 @@
 export default cookieReader = () => 
 {
-  
+  let onKey = true;
+  let kvalue = "";
+  let vvalue = "";
+  let retval = {};
+  let pass = false;
+  for(let i in document.cookie)
+  {
+    if(i == '=' && onKey == true && !pass)
+    {
+      onKey = false;
+    }
+    else if(i == ';' && !pass)
+    {
+      onKey = true;
+    }
+  }
 };
