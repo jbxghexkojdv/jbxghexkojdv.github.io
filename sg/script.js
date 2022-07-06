3d536ce78ba7bfbba2cc670d60fe5cf2db3b680f
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-07-06 13:27:15 -0500

    Update script.js

--- sg/script.js
+++ sg/script.js
@@ -1,3 +1,3 @@
 let x = setInterval(() => {
-  document.getElementById("number").innerHTML = (new Date("Jul 11, 2022 15:45:00") - new Date()).toString(2);
+  document.getElementById("number").innerHTML = Math.round((new Date("Jul 11, 2022 15:45:00") - new Date())/1000).toString(2);
 }, 200);
