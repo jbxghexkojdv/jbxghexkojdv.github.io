e3378d98740388849b25b7a2706d5438265f81e8
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-07-22 13:50:36 -0500

    Update traductor.js

--- traductor.js
+++ traductor.js
@@ -2,7 +2,7 @@ function translate(elem, lang)
 {
   const langids = ["en", "es"];
   const notFounds = ["Translation not found", "Traducción no encontrado"];
-  const phrases = [["Contact me", "The school-renowned Period Timer (v1.11.2)"], ["Me contactar", "El reloj de los períodos que es renombrado en todo de la escuela (v1.11.2)"]];
+  const phrases = [["Contact me", "The school-renowned Period Timer (v1.11.2)", "Crazy -ss Sentences"], ["Me contactar", "El reloj de los períodos que es renombrado en todo de la escuela (v1.11.2)", "Oraciones locas"]];
   lang = lang.slice(0, 2);
   if(lang == "en")
   {
