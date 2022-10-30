132747563d2a39d394ebf708e9957c3fc3d6e33a
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-10-29 23:24:28 -0500

    update script to do a thing

--- 2022elections/election.js
+++ 2022elections/election.js
@@ -28,6 +28,11 @@ function updateBars(data)
         displayObj[dataInner.display[i]].style.height = "100%";
         displayObj[dataInner.display[i]].style.width = `${dataInner[dataInner.display[i]].amount*100/dataInner.total}%`;
         displayObj[dataInner.display[i]].style.backgroundColor = dataInner[dataInner.display[i]].color;
+        displayObj[dataInner.display[i]].style.color = 
+        ((parseInt(dataInner[dataInner.display[i]].color[1] + dataInner[dataInner.display[i]].color[2], 16) * 0.2126) +
+        (parseInt(dataInner[dataInner.display[i]].color[3] + dataInner[dataInner.display[i]].color[4], 16) * 0.7152) + 
+        (parseInt(dataInner[dataInner.display[i]].color[5] + dataInner[dataInner.display[i]].color[6], 16) * 0.0722))
+         > 127.5 ? "#000000" : "#FFFFFF";
         displayObj[dataInner.display[i]].style.display = "block";
         displayObj[dataInner.display[i]].innerHTML = dataInner[dataInner.display[i]].amount;
         if(afterPipe)
@@ -67,48 +72,44 @@ function updateBars(data)
   return true;
 }
 
-
-
-function execute()
+const difference = new Date("Nov 8, 2022 17:00:00")-Date.now();
+const days = zeroify(Math.floor(difference/86400000));
+const hours = zeroify(Math.floor((difference%86400000)/3600000));
+const minutes = zeroify(Math.floor((difference%3600000)/60000));
+const seconds = zeroify(Math.floor((difference%60000)/1000));
+if(difference > 0)
 {
-    const difference = new Date("Nov 8, 2022 17:00:00")-Date.now();
-    const days = zeroify(Math.floor(difference/86400000));
-    const hours = zeroify(Math.floor((difference%86400000)/3600000));
-    const minutes = zeroify(Math.floor((difference%3600000)/60000));
-    const seconds = zeroify(Math.floor((difference%60000)/1000));
-    if(difference > 0)
-    {
-      document.getElementById("g").style.display = "none";
-      document.getElementById("s").style.display = "none";
-      document.getElementById("h").style.display = "none";
-      document.getElementById("middleLine").style.display = "none";
-      document.getElementById("time").style.fontSize = `225px`;
-      document.getElementById("time").style.top = `${(0.5*screen.availHeight)-402.5}px`;
-      outputElement.innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
-    }
-    else
-    {
-      window.req = new XMLHttpRequest();
-      req.onload = () => 
-      {
-        if(updateBars(JSON.parse(req.responseText)))
-        {
-          outputElement.style.fontSize = "10px";
-          outputElement.style.top = "0%";
-          outputElement.style.left = "0%";
-          outputElement.style.textAlign = "left";
-          document.getElementById("g").style.display = "block";
-          document.getElementById("s").style.display = "block";
-          document.getElementById("h").style.display = "block";
-          document.getElementById("middleLine").style.display = "block";
-          outputElement.innerHTML = new Date();
-        }
-      };
-      req.open("GET", "https://jbxghexkojdv.github.io/2022elections/election.json");
-      req.send();
-      delete window.req;
-    }
+  document.getElementById("g").style.display = "none";
+  document.getElementById("s").style.display = "none";
+  document.getElementById("h").style.display = "none";
+  document.getElementById("middleLine").style.display = "none";
+  document.getElementById("time").style.fontSize = `225px`;
+  document.getElementById("time").style.top = `${(0.5*screen.availHeight)-402.5}px`;
+  outputElement.innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
 }
-
-let int = setInterval(execute, 1000);
-execute();
+else
+{
+  let req = new XMLHttpRequest();
+  req.onload = () => 
+  {
+    if(updateBars(JSON.parse(req.responseText)))
+    {
+      outputElement.style.fontSize = "10px";
+      outputElement.style.top = "0%";
+      outputElement.style.left = "0%";
+      outputElement.style.textAlign = "left";
+      document.getElementById("g").style.display = "block";
+      document.getElementById("s").style.display = "block";
+      document.getElementById("h").style.display = "block";
+      document.getElementById("middleLine").style.display = "block";
+      outputElement.innerHTML = new Date();
+    }
+  };
+  req.open("GET", "https://jbxghexkojdv.github.io/2022elections/election.json");
+  req.send();
+  setTimeout(
+  function(){
+    location.replace(location.href);
+  },
+  60000);
+}
