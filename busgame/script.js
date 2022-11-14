e6d3459c1926bbba8821e073cef9e2ac968ce467
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-11-13 18:06:05 -0600

    test notification

--- busgame/script.js
+++ busgame/script.js
@@ -1,1 +1,18 @@
-//e
+navigator.serviceWorker.register('sw.js');
+
+function notify(challenge) {
+  Notification.requestPermission((result) => {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification(challenge.name, {
+          body: challenge.desc,
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}
+
+notify(generateChallenge());
